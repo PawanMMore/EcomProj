90ffd9bba9b1c922485964271b6a28e7afef304d
pawanmm <pawanmm.2011@gmail.com>
2026-04-07 20:19:15 +0530

    Integrating ProdcutCatalog Database and establibishing connection

--- src/main/java/com/pawan/productcatalogservice/models/Product.java
+++ src/main/java/com/pawan/productcatalogservice/models/Product.java
@@ -3,16 +3,22 @@ package com.pawan.productcatalogservice.models;
 import com.pawan.productcatalogservice.dtos.CategoryDTO;
 import com.pawan.productcatalogservice.dtos.FakestoreProductDto;
 import com.pawan.productcatalogservice.dtos.ProductDTO;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private  String productName;
     private String productDescription;
     private Double productPrice;
     private String imageURL;
+
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 
     //conver product to fakstoreProductDto
